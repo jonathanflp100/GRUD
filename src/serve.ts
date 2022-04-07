42d438066bf750681da33c12d056365b2121d833
jonathanflp100 <jonathanflp100@gmail.com>
2022-04-07 17:25:33 -0300

    first commit

---
+++ src/serve.ts
@@ -0,0 +1,67 @@
+import express, { response }  from 'express'
+import {v4 as uuid} from 'uuid'
+const app = express()
+
+app.use(express.json())
+
+interface User {
+  id: string
+  nome: string
+  email: string
+}
+const users : User [] = []
+
+app.get('/users', (request, response) =>{
+  return response.json(users)
+})
+
+
+app.post('/users', (request, response) => {
+const {nome,email} = request.body
+
+const user = {id: uuid(), nome, email}
+
+users.push(user)
+
+return response.json(user)
+ 
+ 
+})
+
+
+app.put('/users/:id', (request, response) => {
+  const {id} = request.params
+  const {nome,email} = request.body
+   
+  const userIndex = users.findIndex((user) => user.id === id)
+
+  if (userIndex < 0){
+    return response.status(404).json({ error: 'User not found.'})
+
+    const user = {id, nome, email}
+
+    users[userIndex] = user
+
+    return response.json(user)
+
+  }
+
+})
+
+app.delete('/users/id:', (request, response) => {
+  const {id} = request.params
+
+  const userIndex = users.findIndex((user) => user.id === id)
+
+  if (userIndex < 0){
+    return response.status(404).json({ error: 'User not found.'})
+
+  users.splice (userIndex, 1)
+     
+  return response.status(204).send()  
+
+})
+
+app.listen('3333', () =>{
+  console.log('back-end started!')
+}) 
